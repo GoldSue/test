e4d9c39c6b1c2b273236e11e84ce7bf0c601ca5a
libaojin <1497274207@qq.com>
2025-01-01 09:40:52 +0800

    ff

--- test.py
+++ test.py
@@ -1,7 +1,31 @@
-# 使用lambda表达式创建匿名平方函数
-squared_numbers = map(lambda x: x ** 2, [1, 2, 3, 4, 5])
-print(squared_numbers)
+from multiprocessing import Process
+from os import getpid
+from random import randint
+from time import time, sleep
+import mysql.connector
 
-print(list(squared_numbers))  # 输出：[1, 4, 9, 16, 25]
+conn = mysql.connector.connect(host='localhost', user='root', password='123456', database='test')
 
 
+def download_task(filename):
+    print('启动下载进程，进程号[%d].' % getpid())
+    print('开始下载%s...' % filename)
+    time_to_download = randint(5, 10)
+    sleep(time_to_download)
+    print('%s下载完成! 耗费了%d秒' % (filename, time_to_download))
+
+
+def main():
+    start = time()
+    p1 = Process(target=download_task, args=('Python从入门到住院.pdf', ))
+    p1.start()
+    p2 = Process(target=download_task, args=('Peking Hot.avi', ))
+    p2.start()
+    p1.join()
+    p2.join()
+    end = time()
+    print('总共耗费了%.2f秒.' % (end - start))
+
+
+if __name__ == '__main__':
+    main()
